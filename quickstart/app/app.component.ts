dc87ea51afddb3cc59c44c4d4777f00b0b4100bd
krankuba <krankuba@gmail.com>
2016-10-23 11:29:35 +0300

    updated content

---
+++ quickstart/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<h3>Minimal Angular 2 App</h3>
+               <p> yohoho its fuckin working </p>
+              `
+})
+export class AppComponent { }
