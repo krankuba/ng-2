ffb0b1fc34995b4499b07ea7236030d2d8b63d21
krankuba <krankuba@gmail.com>
2016-10-23 17:23:51 +0300

    new content

--- race-app/app/race-model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var RaceModel = (function () {
-    function RaceModel() {
-    }
-    return RaceModel;
-}());
-exports.RaceModel = RaceModel;
-//# sourceMappingURL=race-model.js.map
