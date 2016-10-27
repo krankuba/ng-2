a3215725122286f11bfc95724e27a4422cd86836
krankuba <krankuba@gmail.com>
2016-10-27 20:51:33 +0300

    new content

---
+++ visionr-test-mini/app/about/about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    moduleId: module.id,
+    selector: 'my-about',
+    templateUrl: '/app/about/about.component.html',
+    styleUrls: [ '../../app/about/about.component.css' ]
+})
+
+export class AboutComponent {
+
+}
---
+++ visionr-test-mini/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    moduleId: module.id,
+    selector: 'my-home',
+    templateUrl: '/app/home/home.component.html',
+    styleUrls: [ '../../app/home/home.component.css' ]
+})
+
+export class HomeComponent {
+
+}
